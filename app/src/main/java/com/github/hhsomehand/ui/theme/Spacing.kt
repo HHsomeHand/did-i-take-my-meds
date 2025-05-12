2cc946bb2b407bd955b0b82ab0e31892281c187f
宫仅 <8281538+HHandHsome@user.noreply.gitee.com>
2025-05-12 18:01:13 +0800

    feat: 添加了 Spacing.kt 来统一管理 Spacer 的间距

---
+++ app/src/main/java/com/github/hhsomehand/ui/theme/Spacing.kt
@@ -0,0 +1,10 @@
+package com.github.hhsomehand.ui.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+enum class Spacing(val value: Dp) {
+    SMALL(8.dp),
+    MEDIUM(16.dp),
+    LARGE(24.dp)
+}
