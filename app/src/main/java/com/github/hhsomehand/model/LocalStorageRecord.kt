7db58e4cbcfb2c2b1876cf3d3eb2e1f885a62eb9
宫仅 <8281538+HHandHsome@user.noreply.gitee.com>
2025-05-18 14:04:08 +0800

    refactor: 将 PrefsUtils 完整地重构了

---
+++ app/src/main/java/com/github/hhsomehand/model/LocalStorageRecord.kt
@@ -0,0 +1,3 @@
+package com.github.hhsomehand.model
+
+data class LocalStorageRecord<T>(val key: String, val value: T)
--- app/src/main/java/com/github/hhsomehand/utils/LocalStorage.kt
+++ app/src/main/java/com/github/hhsomehand/utils/LocalStorage.kt
@@ -1,10 +1,23 @@
 package com.github.hhsomehand.utils
 
 import android.content.Context
+import com.github.hhsomehand.MyApplication
 import com.github.hhsomehand.constant.PrefsConst
+import com.github.hhsomehand.model.LocalStorageRecord
 
 object LocalStorage {
-    fun <T> get(context: Context, key: String, defaultValue: T): T {
+    fun <T> get(
+        record: LocalStorageRecord<T>,
+        context: Context = MyApplication.instance.applicationContext
+    ): T {
+        return get(context, record.key, record.value)
+    }
+
+    fun <T> get(
+        key: String,
+        defaultValue: T,
+        context: Context = MyApplication.instance.applicationContext
+    ): T {
         var prefs = context.getSharedPreferences(PrefsConst.appStoreName, Context.MODE_PRIVATE)
 
         return when (defaultValue) {
@@ -15,5 +28,12 @@ object LocalStorage {
             is Float -> prefs.getFloat(key, defaultValue) as T
             else -> throw Exception("PrefsUtils.get 只能记录基本数据类型")
         }
+    }
+
+    /**
+     * 遗弃的函数, 不要使用
+     */
+    fun <T> get(context: Context, key: String, defaultValue: T): T {
+        return get(key, defaultValue, context)
     }
 }
